d3b4c589f5165292739716b7364ac9e02c75fc57
wswenyue <wswenyue@163.com>
2024-03-18 17:33:09 +0800

    first commit

--- Formula/ioTools.rb
+++ Formula/ioTools.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class IoTools < Formula
+class Iotools < Formula
   desc "ioTools using rust build."
   homepage "https://github.com/wswenyue/ioTools"
   url "https://github.com/wswenyue/ioTools/releases/download/v0.1.0.6/io_tools-0.1.0-x86_64-apple-darwin.tar.gz"
@@ -9,7 +9,7 @@ class IoTools < Formula
   version '0.1.0.6'
 
   def install
-    bin.install "ioTools"
+    bin.install "iotools"
   end
 
   test do
